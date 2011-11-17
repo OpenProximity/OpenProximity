b6362045fa019879f32ec098fb8004f6951ed3ce
Manuel Francisco Naranjo <manuel@aircable.net>
2011-11-17 02:10:11 -0800

    Reflect some of the variables name changes

--- src/openproximity/context_processors.py
+++ src/openproximity/context_processors.py
@@ -5,7 +5,7 @@ required
 import os
 from django.conf import settings
 try:
-    from openproximity import version as opversion
+    from openproximity import __version__ as opversion
 except:
     opversion = 'ND'
 
@@ -13,9 +13,9 @@ def variables(requesst):
     return {
         'version': { 'current': opversion },
         'settings': {
-            'logo': settings.OP2_LOGO,
-            'debug': settings.OP2_DEBUG,
-            'translate': settings.OP2_TRANSLATE,
-            'twitter': settings.OP2_TWITTER
+            'logo': settings.OP_LOGO,
+            'debug': settings.OP_DEBUG,
+            'translate': settings.OP_TRANSLATE,
+            'twitter': settings.OP_TWITTER
         }
     }
