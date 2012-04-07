873c88d40a0c87a550a65c04ba3b21f7be13cfec
Manuel Francisco Naranjo <manuel@aircable.net>
2012-04-07 12:43:51 -0700

    Adding upstart installer script

---
+++ upstart/install.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+if [ -z "${1}" ]; then
+    echo "Usage ${0} <openproximity path>"
+    exit 1
+fi
+
+URL="https://raw.github.com/OpenProximity/OpenProximity/master/"
+RPATH="/opt/openproximity"
+OPATH="${1}"
+
+OPATH="echo ${OPATH} | sed -e 's/\//\\\//g'"
+RPATH="echo ${RPATH} | sed -e 's/\//\\\//g'"
+
+cd /etc/init
+wget ${URL}/upstart/bluetoothd.conf
+wget ${URL}/upstart/openproximity-web.conf
+wget ${URL}/upstart/openproximity-rpc.conf
+wget ${URL}/upstart/openproximity-rpc-scanner.conf
+wget ${URL}/upstart/openproximity-rpc-uploader.conf
+sed -i "s/${RPATH}/${OPATH}/g" openproximity-web.conf
+sed -i "s/${RPATH}/${OPATH}/g" openproximity-rpc.conf
+sed -i "s/${RPATH}/${OPATH}/g" openproximity-rpc-scanner.conf
+sed -i "s/${RPATH}/${OPATH}/g" openproximity-rpc-uploader.conf
